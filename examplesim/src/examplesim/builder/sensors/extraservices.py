984cff47b3f720216ff1d24b891549724d963e2c
Pierrick Koch <pierrick.koch@gmail.com>
2016-01-29 16:58:40 +0100

    [examplesim] replace init by _classpath
    fixed builder bug in morse 1.3-190-gd7496
    
    ...
        obj.game.physics_type = 'NO_COLLISION'
    AttributeError: 'NoneType' object has no attribute 'game'

--- examplesim/src/examplesim/builder/sensors/extraservices.py
+++ examplesim/src/examplesim/builder/sensors/extraservices.py
@@ -1,7 +1,4 @@
 from morse.builder.creator import SensorCreator
 
 class ExtraServices(SensorCreator):
-    def __init__(self, name=None):
-        SensorCreator.__init__(self, name, \
-                               "examplesim.sensors.extraservices.ExtraServices",\
-                               "ExtraServices")
+    _classpath = "examplesim.sensors.extraservices.ExtraServices"
